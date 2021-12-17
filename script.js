01df8d105a73e3537ca8cd33ec8b06a43ddc6844
LaiseFrance <laise94@hotmail.com>
2021-12-17 20:45:27 -0300

    adicionando alteraçoes

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let black = document.getElementsByClassName('color')[0].style.background = 'black';
+let secondcolor = document.querySelectorAll('.color')[1].style.background = 'purple'
+let thirdcolor = document.querySelectorAll('.color')[2].style.background = 'pink'
+let fourthcolor = document.querySelectorAll('.color')[3].style.background = 'blue'
+
+
+
+
+
+
+
+
+
+
