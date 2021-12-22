39bebb3a391db30aa0106abf9befdd7cec9cb748
LaiseFrance <laise94@hotmail.com>
2021-12-22 16:44:01 -0300

    alinhando elementos2

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 
 //cores da paleta de cores
+let black = document.getElementsByClassName('color')[0].style.background = 'black';
+let secondcolor = document.querySelectorAll('.color')[1].style.background = 'rgb(227, 233, 236)'
+let thirdcolor = document.querySelectorAll('.color')[2].style.background = 'rgb(251, 5, 46)'
+let fourthcolor = document.querySelectorAll('.color')[3].style.background = 'rgb(7, 81, 165)'
+
+
+
+
 
 
 //colorindo pixel cor preta como cor de entrada
