f116680d38c3f11bc650cfd78f74553fff20ddbf
LaiseFrance <laise94@hotmail.com>
2021-12-21 22:28:28 -0300

    adicionando alteraçoes, ainda a serem corrigidas

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 
+//cores da paleta de cores
 let black = document.getElementsByClassName('color')[0].style.background = 'black';
-let secondcolor = document.querySelectorAll('.color')[1].style.background = 'purple'
-let thirdcolor = document.querySelectorAll('.color')[2].style.background = 'pink'
-let fourthcolor = document.querySelectorAll('.color')[3].style.background = 'blue'
+let secondcolor = document.querySelectorAll('.color')[1].style.background = 'rgb(227, 233, 236)';
+let thirdcolor = document.querySelectorAll('.color')[2].style.background = 'rgb(251, 5, 46)';
+let fourthcolor = document.querySelectorAll('.color')[3].style.background = 'rgb(7, 81, 165)';
+
+
+//colorindo pixel cor preta como cor de entrada
+
+function colorirpixel (pixel) {
+pixel.style.background= pencolour;
+}
+
+let pencolour= 'black'
+function setPenColour(color)
+{
+ pencolour=color;
+}
 
 
 
 
-
-
-
-
-
-
+//criar uma função para selecionar as cores quando clicar
