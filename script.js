c3f62ff98ad2066ad8eeff7b283338ba81a51406
LaiseFrance <laise94@hotmail.com>
2021-12-22 18:48:16 -0300

    modificando quadro de pixels

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 
 //cores da paleta de cores
 let black = document.getElementsByClassName('color')[0].style.background = 'black';
-let secondcolor = document.querySelectorAll('.color')[1].style.background = 'rgb(227, 233, 236)'
-let thirdcolor = document.querySelectorAll('.color')[2].style.background = 'rgb(251, 5, 46)'
-let fourthcolor = document.querySelectorAll('.color')[3].style.background = 'rgb(7, 81, 165)'
-
-
-
-
-
+let secondcolor = document.querySelectorAll('.color')[1].style.background = 'rgb(227, 233, 236)';
+let thirdcolor = document.querySelectorAll('.color')[2].style.background = 'rgb(251, 5, 46)';
+let fourthcolor = document.querySelectorAll('.color')[3].style.background = 'rgb(7, 81, 165)';
 
 //colorindo pixel cor preta como cor de entrada
 
@@ -21,8 +16,5 @@ function setPenColour(color)
 {
  pencolour=color;
 }
-
-
-
 
 //criar uma função para selecionar as cores quando clicar
