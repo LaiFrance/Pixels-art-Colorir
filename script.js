b73b0c174fee49b62ec8ec1dd306766feb4e9d08
LaiseFrance <laise94@hotmail.com>
2021-12-22 22:45:57 -0300

    alterei o script.js para color selected

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 //cores da paleta de cores
-let black = document.getElementsByClassName('color')[0].style.background = 'black';
+let black = document.getElementsByClassName('color selected')[0].style.background = 'black';
 let secondcolor = document.querySelectorAll('.color')[1].style.background = 'rgb(227, 233, 236)';
 let thirdcolor = document.querySelectorAll('.color')[2].style.background = 'rgb(251, 5, 46)';
 let fourthcolor = document.querySelectorAll('.color')[3].style.background = 'rgb(7, 81, 165)';
