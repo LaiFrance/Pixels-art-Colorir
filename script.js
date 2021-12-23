252268f03b725acd28c6a8d18d7fa7d6bc973c02
LaiseFrance <laise94@hotmail.com>
2021-12-23 19:03:48 -0300

    adicionando funçao ao botao limpar

--- script.js
+++ script.js
@@ -7,14 +7,28 @@ let fourthcolor = document.querySelectorAll('.color')[3].style.background = 'rgb
 
 //colorindo pixel cor preta como cor de entrada
 
-function colorirpixel (pixel) {
-pixel.style.background= pencolour;
+function colorirpixel(pixel) {
+ pixel.style.background = pencolour;
 }
 
-let pencolour= 'black'
-function setPenColour(color)
-{
- pencolour=color;
+let pencolour = 'black'
+function setPenColour(color) {
+ pencolour = color;
 }
 
 //criar uma função para selecionar as cores quando clicar
+
+
+//botao limpar
+ botaolimpar=document.getElementById("clear-board")
+function limparcores(){
+const pixels= document.querySelectorAll(".pixel");
+ for (let i=0;i<pixels.length;i+=1){
+  pixels[i].style.backgroundColor = 'rgb(255, 255, 255)';
+ }
+}
+botaolimpar.addEventListener("click",limparcores)
+
+
+
+
