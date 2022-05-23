8a677d0929dff1d34eb637add2f6076501546e02
Laise France <laise94@hotmail.com>
2022-05-23 09:26:07 -0300

    modificando

--- script.js
+++ script.js
@@ -1,43 +1,120 @@
+const colorPallet = document.querySelectorAll('#color-palette div');
+const grid = document.querySelector('#pixel-board');
+const pixels = document.getElementsByClassName('pixel');
+const clearBoard = document.getElementById('clear-board');
+const boardSize = document.getElementById('board-size');
+const vqvButton = document.getElementById('generate-board');
+const generatingColor = document.getElementById('generate-color');
+let nColorB = document.getElementById('color2');
+let nColorC = document.getElementById('color3');
+let nColorD = document.getElementById('color4');
+let colorRgb = [];
 
-//cores da paleta de cores
-let black = document.getElementsByClassName('color')[0].style.background = 'black';
-let gray = document.querySelectorAll('.color')[1].style.background = 'gray'
-let red = document.querySelectorAll('.color')[2].style.background = 'red'
-let blue = document.querySelectorAll('.color')[3].style.background = 'blue'
 
+function pickColor(event) {
+  for (let index = 0; index < colorPallet.length; index += 1) {
+    if (colorPallet[index].className === 'color selected') {
+      colorPallet[index].className = 'color';
+      break;
+    }
+  }
+  event.target.className = 'color selected';
 
-
-//colorindo pixels (cor preta como cor de entrada)
-
-function colorirpixel(pixel) {
- pixel.style.background = pencolour;
 }
 
-let pencolour = 'black'
-function setPenColour(color) {
- pencolour = color;
+for (let i = 0; i < colorPallet.length; i += 1) {
+  colorPallet[i].addEventListener('click', pickColor);
+}
+function painting(event) {
+  event.target.style.backgroundColor = window.getComputedStyle(document.getElementsByClassName('color selected')[0]).getPropertyValue('background-color');
 }
 
-//criar uma função para selecionar as cores quando clicar
-
-
-
-
-//botao limpar
- botaolimpar=document.getElementById("clear-board")//localizar id
- //criar função
-function limparcores(){
-const pixels= document.querySelectorAll(".pixel");//seleciona pixels
- for (let i=0;i<pixels.length;i+=1){ //percorrer pixels
-  pixels[i].style.backgroundColor = 'rgb(255, 255, 255)';// cor branca 
- }
+for (let i = 0; i < pixels.length; i += 1) {
+  grid.addEventListener('click', painting);
 }
-botaolimpar.addEventListener("click",limparcores) // adicionar evento (depois olhar pq o value não funcionou e pesquisar sobre o botão reset)
+function clear() {
 
-
-
-//alterar cores
-botaocores = document.getElementsByClassName ("mudarcores");
-function escolhercores(){
- 
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].style.backgroundColor = 'white';
+  }
 }
+
+clearBoard.addEventListener('click', clear);
+
+
+function intervalCheck() {
+  if (boardSize.value <= 5 && boardSize.value !== '') {
+    boardSize.value = 5;
+  } else if (boardSize.value >= 50) {
+    boardSize.value = 50;
+  }
+}
+
+function eraser(event) {
+  event.target.style.backgroundColor = 'white';
+}
+
+for (let i = 0; i < pixels.length; i += 1) {
+  grid.addEventListener('dblclick', eraser);
+}
+
+function createBoard() {
+  if (boardSize.value === '') {
+    alert('Board inválido!')
+  }
+
+  grid.innerHTML = '';
+
+  intervalCheck();
+
+  grid.style.width = 42 * boardSize.value + 'px';
+  for (let index = 0; index < boardSize.value * boardSize.value; index += 1) {
+    let pixel = document.createElement('div');
+    pixel.className = 'pixel';
+    grid.appendChild(pixel);
+  }
+}
+
+vqvButton.addEventListener('click', createBoard);
+
+function randomNumbers() {
+  let rgb = []
+  for (let i = 0; i < 3; i += 1) {
+    rgb.push(Math.floor(Math.random() * 255) + 1)
+  }
+
+  return 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')'
+}
+
+function randomColors() {
+  for (let index = 0; index < 3; index += 1) {
+    let color = randomNumbers();
+
+    if (index === 0) {
+      colorRgb.push(color);
+    }
+    if (index === 1 && color !== colorRgb[0]) {
+      colorRgb.push(color)
+    } else if (index === 1) {
+      colorRgb.push(randomNumbers())
+    }
+    if (index === 2 && color !== colorRgb[0] && color !== colorRgb[1]) {
+      colorRgb.push(color)
+    } else if (index === 2) {
+      colorRgb.push(randomNumbers())
+    }
+  }
+}
+
+function palletGenerator() {
+  colorRgb = [];
+
+  randomColors();
+
+  nColorB.style.backgroundColor = colorRgb[0]
+  nColorC.style.backgroundColor = colorRgb[1]
+  nColorD.style.backgroundColor = colorRgb[2]
+}
+generatingColor.addEventListener('click', palletGenerator)
+
+window.onload = palletGenerator
